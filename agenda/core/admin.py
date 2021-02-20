9eeb8e46164220ec836c0092f1254321e8097af2
clesiomatias <clesiofmatias@gmail.com>
2021-02-19 21:16:16 -0300

    Mediano

---
+++ agenda/core/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Evento
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display=('titulo','data_evento','data_criacao')
+    list_filter = ('titulo','data_evento',)
+admin.site.register(Evento, EventoAdmin)
---
+++ agenda/core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.lista_eventos),
+    path('agenda/' , views.lista_eventos),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
+    
+]
